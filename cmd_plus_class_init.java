619dad3d2cc3cb920ab45b10a7c8d574a96072ee
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-16 15:33:39 +0300

    Create cmd_plus_class_init.java

---
+++ cmd_plus_class_init.java
@@ -0,0 +1,17 @@
+public class CommandLine {
+
+    public static void main(String[] args) {
+        Done done = new Done();
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]: " +  args[i]);
+        }
+        System.out.println(done.toString());
+    }
+}
+
+class Done {
+    String x;
+    Done() {
+        x = "Done!";
+    }
+}
